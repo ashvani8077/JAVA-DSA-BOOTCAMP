5f9f1ee65965e4267e503213e36bbda8fb8634c4
ashvani8077 <122486071+ashvani8077@users.noreply.github.com>
2024-01-09 22:35:11 +0530

    Add files via upload

---
+++ LeetCodeJumpArray.java
@@ -0,0 +1,25 @@
+//Example 1:
+//
+//Input: nums = [2,3,1,1,4]
+//Output: true
+//Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+import java.util.Scanner;
+
+public class LeetCodeJumpArray {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        //class Solution {
+        //    public boolean canJump(int[] nums) {
+        //       int reachable = 0;
+        //       for(int i = 0; i < nums.length; i ++) {
+        //           if(i > reachable) return false;
+        //           reachable = Math.max(reachable, i + nums[i]);
+        //       }
+        //       return true;
+        //    }
+        //}
+
+
+    }
+}
