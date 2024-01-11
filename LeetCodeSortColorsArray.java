47984fa303dc2f8560b0e1f6279ce722aa0adac0
ashvani8077 <122486071+ashvani8077@users.noreply.github.com>
2024-01-11 23:19:59 +0530

    Add files via upload

---
+++ LeetCodeSortColorsArray.java
@@ -0,0 +1,22 @@
+//Example 1:
+//
+//Input: nums = [2,0,2,1,1,0]
+//Output: [0,0,1,1,2,2]
+import java.util.Scanner;
+
+public class LeetCodeSortColorsArray {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+
+
+//class Solution {
+//    public void sortColors(int[] nums) {
+//        Arrays.sort(nums);
+//    }
+//}
+
+
+
+    }
+}
