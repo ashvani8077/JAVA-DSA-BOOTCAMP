7f88832bcbc0bbb8f34d0458ca7e5b3bc0fb81a4
ashvani8077 <122486071+ashvani8077@users.noreply.github.com>
2024-01-12 15:37:52 +0530

    Add files via upload

---
+++ LeetCodeMaximumAchievable.java
@@ -0,0 +1,22 @@
+//Example 1:
+//
+//Input: num = 4, t = 1
+//Output: 6
+//Explanation: The maximum achievable number is x = 6; it can become equal to num after performing this operation:
+//1- Decrease x by 1, and increase num by 1. Now, x = 5 and num = 5.
+//It can be proven that there is no achievable number larger than 6.
+import java.util.Scanner;
+
+public class LeetCodeMaximumAchievable {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        //class Solution {
+        //    public int theMaximumAchievableX(int num, int t) {
+        //        return(t*2)+num;
+        //    }
+        //}
+
+
+    }
+}
---
+++ LeetCodeSquareRoot.java
@@ -0,0 +1,21 @@
+//Example 1:
+//
+//Input: x = 4
+//Output: 2
+//Explanation: The square root of 4 is 2, so we return 2.
+import java.util.Scanner;
+
+public class LeetCodeSquareRoot {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        //class Solution {
+        //    public int mySqrt(int x) {
+        //        int re=(int)Math.pow(x,0.5);
+        //        return re;
+        //    }
+        //}
+
+
+    }
+}
