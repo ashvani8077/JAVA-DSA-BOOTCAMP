07203d2ce1223fd0996e79cbbd9ad6a639d18e3e
ashvani8077 <122486071+ashvani8077@users.noreply.github.com>
2024-01-13 20:59:32 +0530

    Add files via upload

---
+++ LeetCodeCountItemsMatchingRule.java
@@ -0,0 +1,39 @@
+//Example 1:
+//
+//Input: items = [["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]], ruleKey = "color", ruleValue = "silver"
+//Output: 1
+//Explanation: There is only one item matching the given rule, which is ["computer","silver","lenovo"].
+import java.util.Scanner;
+
+public class LeetCodeCountItemsMatchingRule {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+
+        //class Solution {
+        //    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        //        int ind=0; int c=0;
+        //        if(ruleKey.equals("type")){
+        //            ind=1;
+        //        }
+        //        else if(ruleKey.equals("color")){
+        //            ind=2;
+        //        }
+        //        else{
+        //            ind=3;
+        //        }
+        //        for(int i=0;i<items.size();i++){
+        //           if( items.get(i).get(ind-1).equals(ruleValue)){
+        //                c++;
+        //            }
+        //        }
+        //        return c;
+        //    }
+        //}
+
+
+
+
+
+    }
+}
---
+++ LeetCodeNumberOfEmployeeTarget.java
@@ -0,0 +1,40 @@
+//Example 1:
+//
+//Input: hours = [0,1,2,3,4], target = 2
+//Output: 3
+//Explanation: The company wants each employee to work for at least 2 hours.
+//- Employee 0 worked for 0 hours and didn't meet the target.
+//- Employee 1 worked for 1 hours and didn't meet the target.
+//- Employee 2 worked for 2 hours and met the target.
+//- Employee 3 worked for 3 hours and met the target.
+//- Employee 4 worked for 4 hours and met the target.
+//There are 3 employees who met the target.
+//Example 2:
+//
+//Input: hours = [5,1,4,2,2], target = 6
+//Output: 0
+//Explanation: The company wants each employee to work for at least 6 hours.
+//There are 0 employees who met the target.
+import java.util.Scanner;
+
+public class LeetCodeNumberOfEmployeeTarget {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        //class Solution {
+        //    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        //        int c=0;
+        //        for(int i:hours){
+        //            if(i>=target){
+        //                c++;
+        //            }
+        //        }
+        //        return c;
+        //    }
+        //}
+
+
+
+
+    }
+}
