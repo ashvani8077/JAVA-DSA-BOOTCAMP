c6e3a1ac1631c0f65bd7f7be0dad35d63791bf2a
ashvani8077 <122486071+ashvani8077@users.noreply.github.com>
2024-01-16 21:42:53 +0530

    Add files via upload

---
+++ LeetCodeIntersectionOfTwoArray.java
@@ -0,0 +1,39 @@
+//Example 1:
+//
+//Input: nums1 = [1,2,2,1], nums2 = [2,2]
+//Output: [2]
+import java.util.Scanner;
+
+public class LeetCodeIntersectionOfTwoArray {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        //class Solution {
+        //       public static int[] intersection(int[] nums1, int[] nums2)
+        //    {
+        //        HashSet<Integer> set1 = new HashSet<>();
+        //        HashSet<Integer> set2 = new HashSet<>();
+        //
+        //        for (int n : nums1) {
+        //            set1.add(n);
+        //        }
+        //
+        //        for (int n : nums2) {
+        //            if (set1.contains(n))
+        //                set2.add(n);
+        //        }
+        //
+        //        int [] result = new int[set2.size()];
+        //            int index = 0;
+        //        for (int n : set2){
+        //            result[index++] = n;
+        //        }
+        //
+        //        return result;
+        //    }
+        //
+        //}
+
+
+    }
+}
