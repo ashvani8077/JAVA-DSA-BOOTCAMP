8c8b90d729e837e1155aeb8f1f87bc976bff7950
ashvani8077 <122486071+ashvani8077@users.noreply.github.com>
2024-01-17 23:38:24 +0530

    Add files via upload

---
+++ LeetCodeIntersectionOfTwoArray2.java
@@ -0,0 +1,58 @@
+//Example 1:
+//
+//Input: nums1 = [1,2,2,1], nums2 = [2,2]
+//Output: [2,2]
+//Example 2:
+//
+//Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+//Output: [4,9]
+//Explanation: [9,4] is also accepted.
+import java.util.Scanner;
+
+public class LeetCodeIntersectionOfTwoArray2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        //class Solution {
+        //    public int[] intersect(int[] nums1, int[] nums2) {
+        //        // Sort both the arrays first...
+        //        Arrays.sort(nums1);
+        //        Arrays.sort(nums2);
+        //        // Create an array list...
+        //        ArrayList<Integer> arr = new ArrayList<Integer>();
+        //        // Use two pointers i and j for the two arrays and initialize both with zero.
+        //        int i = 0, j = 0;
+        //        while(i < nums1.length && j < nums2.length){
+        //            // If nums1[i] is less than nums2[j]...
+        //            // Leave the smaller element and go to next(greater) element in nums1...
+        //            if(nums1[i] < nums2[j]) {
+        //                i++;
+        //            }
+        //            // If nums1[i] is greater than nums2[j]...
+        //            // Go to next(greater) element in nums2 array...
+        //            else if(nums1[i] > nums2[j]){
+        //                j++;
+        //            }
+        //            // If both the elements intersected...
+        //            // Add this element to arr & increment both i and j.
+        //            else{
+        //                arr.add(nums1[i]);
+        //                i++;
+        //                j++;
+        //            }
+        //        }
+        //        // Create a output list to store the output...
+        //        int[] output = new int[arr.size()];
+        //        int k = 0;
+        //        while(k < arr.size()){
+        //            output[k] = arr.get(k);
+        //            k++;
+        //        }
+        //        return output;
+        //    }
+        //}
+
+
+
+    }
+}
