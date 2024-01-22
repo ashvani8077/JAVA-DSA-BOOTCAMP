72a30f73d19f31a6052a4808d3c8cdc59110e809
ashvani8077 <122486071+ashvani8077@users.noreply.github.com>
2024-01-22 19:43:07 +0530

    Add files via upload

---
+++ LeetCodeCheckDoubleExists.java
@@ -0,0 +1,25 @@
+//Input: arr = [10,2,5,3]
+//Output: true
+//Explanation: For i = 0 and j = 2, arr[i] == 10 == 2 * 5 == 2 * arr[j]
+import java.util.Scanner;
+
+public class LeetCodeCheckDoubleExists {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+//        class Solution {
+//            public boolean checkIfExist(int[] arr) {
+//                int n = arr.length;
+//                for (int i = 0; i < n; i++)
+//                    for (int j = 0; j < n; j++)
+//                        if (i != j && arr[i] == 2 * arr[j])
+//                            return true;
+//
+//                return false;
+//            }
+//        }
+
+
+
+    }
+}
